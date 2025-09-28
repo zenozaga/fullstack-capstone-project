77556ffda8963a941d2df92d7b46bf6fd04de3a9
Randy stiven Valentin <zenozaga@gmail.com>
2025-09-28 01:34:48 -0400

    Adding Search Router

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -1,28 +1,32 @@
 // db.js
-require('dotenv').config();
-const MongoClient = require('mongodb').MongoClient;
+require("dotenv").config();
+const MongoClient = require("mongodb").MongoClient;
 
 // MongoDB connection URL with authentication options
 let url = `${process.env.MONGO_URL}`;
 
+
+/**
+ * @type {import('mongodb').Db}
+ */
 let dbInstance = null;
 const dbName = "giftdb";
 
 async function connectToDatabase() {
-    if (dbInstance){
-        return dbInstance
-    };
+  if (dbInstance) {
+    return dbInstance;
+  }
 
-    const client = new MongoClient(url);      
+  const client = new MongoClient(url);
 
-    // Task 1: Connect to MongoDB
-    // {{insert code}}
+  // Task 1: Connect to MongoDB
+  await client.connect();
 
-    // Task 2: Connect to database giftDB and store in variable dbInstance
-    //{{insert code}}
+  // Task 2: Connect to database giftDB and store in variable dbInstance
+  dbInstance = client.db(dbName);
 
-    // Task 3: Return database instance
-    // {{insert code}}
+  // Task 3: Return database instance
+  return dbInstance;
 }
 
 module.exports = connectToDatabase;
