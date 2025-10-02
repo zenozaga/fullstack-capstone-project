b3be8935654ff9594eff01306d705bec6b878080
Randy stiven Valentin <zenozaga@gmail.com>
2025-10-02 06:19:50 -0400

    minor fixes

--- giftlink-backend/middlewares/auth.js
+++ giftlink-backend/middlewares/auth.js
@@ -3,12 +3,11 @@ const logger = require("../logger");
 const JWT_SECRET = process.env.JWT_SECRET;
 
 module.exports = function authMiddleware(req, res, next) {
-  const authHeader = req.header("authorization")?.split(" ")?.[1];
-  if (!authHeader) {
-    return res.status(401).json({ error: "Authorization header missing" });
-  }
+  
+  const token = req.headers.authorization?.split(" ")?.[1];
+  if (!token) return res.status(401).json({ error: "Authorization header missing" });
 
-  jwt.verify(authHeader, JWT_SECRET, (err, user) => {
+  jwt.verify(token, JWT_SECRET, (err, user) => {
     if (err) {
       logger.error("Invalid token", err);
       return res.status(403).json({ error: "Invalid token" });
